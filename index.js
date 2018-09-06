a671764a531786db95a34b60dd099f0479790f6f
tearsofphoenix <tearsofphoenix@icloud.com>
2018-09-06 19:31:36 +0800

    WIP

--- index.js
+++
@@ -1,7 +0,0 @@
-export * as OPCode from './src/opcode'
-
-export * as Tokens from './src/token'
-
-export parse from './src/parser'
-
-export {printOperations, opToString} from './src/util'
---
+++ src/index.js
@@ -0,0 +1,7 @@
+export * as OPCode from './opcode'
+
+export * as Tokens from './token'
+
+export parse from './parser'
+
+export {printOperations, opToString} from './util'
